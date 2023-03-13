c2da69d891782ad99b35ae05d48d8f5401f0960b
luoyu <wzbao76505@163.com>
2023-03-14 07:46:49 +0800

    leetcode:question two

--- src/org/luoyu/day/Question1.java
+++ src/org/luoyu/day/Question1.java
@@ -12,6 +12,9 @@ public final class Question1 {
      * 给定一个一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回他们的数组下标。
      * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
      * 你可以按任意顺序返回答案。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/add-two-numbers
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
 
---
+++ src/org/luoyu/day/Question2.java
@@ -0,0 +1,36 @@
+package org.luoyu.day;
+
+/**
+ * leetcode:第二道题
+ * 两数相加
+ */
+public class Question2 {
+    /**
+     * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。<br/>
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。<br/>
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0开头。<br/>
+     * <br/>
+     * <img width="400px" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2021/01/02/addtwonumber1.jpg"/><br/>
+     *
+     *
+     * 示例 1：<br/>
+     *
+     * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]<br/>
+     * 输出：[8,9,9,9,0,0,0,1]<br/>
+     *
+     *
+     * 提示：<br/>
+     *
+     * 每个链表中的节点数在范围 [1, 100] 内<br/>
+     * 0 <= Node.val <= 9<br/>
+     * 题目数据保证列表表示的数字不含前导零<br/>
+     *
+     * 来源：力扣（LeetCode）<br/>
+     * 链接：https://leetcode.cn/problems/add-two-numbers<br/>
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。<br/>
+     */
+    public static void main(){
+
+    }
+
+}
